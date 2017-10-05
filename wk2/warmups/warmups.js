cf9ff5aeff72d546c1477f423cc875eadc196c0c
michaelvincerra <michael.vincerra@gmail.com>
2017-10-05 14:59:34 -0700

    Adds warmups exercises, including slice(), splice(), and indexOf().

--- wk2/warmups/warmups.js
+++ wk2/warmups/warmups.js
@@ -6,6 +6,8 @@
 
 // NEW METHODS
 // .slice(0,2)
+// .splice() https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+// indexOf()
 // .repeat()
 
 function notString(notty){
@@ -52,6 +54,12 @@ console.log(missingChar('kitten', 1)) // 'ktten'
 console.log(missingChar('kitten', 0)) // 'itten'
 console.log(missingChar('kitten', 4)) // 'kittn'
 
+/////////// MORE PRACTICE ON .slice()  /////////////
+
+
+
+
+
 function front3(three) {
     return three.slice(0,3).repeat(3)
   }
@@ -64,26 +72,50 @@ console.log(stringTimes("Bada", 3) + " BOOM! ")
 
 
 function makeBricks(small, big, goal) {
+// Evaluates to a Boolean, T/F
 // const makeBricks = (small, big, goal) => {}
-// DON'T declare 'const' before variables small, big because the argument is already assigned. 
+// DON'T declare 'const' before variables (small, big) because arguments already assigned in function. 
 small = 1 * small
 big = 5 * big
   if (small + big === goal) {
     return true 
   }else{
     return false 
-}
+  }
 }
 console.log(`The answer is ` + makeBricks(1, 5, 26))
 
 // How do I get subtotal as a variable to print in console.log()?
-//  + {subtotal} + 
+// Could declar as a global variable, but then it doesn't evaluate to 10.
+//  + {subtotal} + ???
 
 function receipt(subtotal) {
   tax_tip = subtotal * 0.24
   return subtotal + tax_tip
 }
 console.log(`Your total includes tax and tip: ` + receipt(10))
+
+
+
+// function findSmallest(a, b, c, d) {
+//   // What is the difference btw Math.min() and Math.min.apply()?
+//   return Math.min(10, 20, 30, 40)
+// }
+// console.log(findSmallest(10, 20, 30, 40))
+
+
+  
+function removeSmallest(numbers) {
+  // Set variable to find minimum value, 
+  // using spread operator ... ES6
+
+  var min = Math.min(...numbers);
+  
+  numbers.splice(numbers.indexOf(min),1);
+  return numbers;
+}
+
+console.log(removeSmallest([10, 20, 30, 40]));
 
 
 
