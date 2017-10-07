98e540591a2d2995bea4262f0eddb2769985966e
michaelvincerra <michael.vincerra@gmail.com>
2017-10-06 21:26:06 -0700

    Makes minor editorial revisions to integration.test.js.

--- wk1/firstApp/src/integration.test.js
+++ wk1/firstApp/src/integration.test.js
@@ -2,49 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 // http://airbnb.io/enzyme/   
 // This test suite uses Enzyme
 /* globals beforeEach describe it expect */
 
 describe('integration testing', () => {
-  let App
+  let app
   beforeEach(() => {
     app = mount(<App/>)
-    })
+  })
 
-it('renders the correct number of filter rows when unfilled', () => {
+  it('renders the correct number of filter rows when unfilled', () => {
     // You must use .render() if you want to find DOM elements ONLY
     // On the other hand you want to search for HTML elements, omit .render()
-    const productRows = app.render().find('.product-rows')
-    expect(true).toBe(true) 
+    const productRows = app.find('ProductRow')
     expect(productRows.length).toBe(6)
-    
-})
+  })
 
-it('It has the right number of category rows', () => {
+  it('renders the right number of category rows', () => {
     const categoryRows = app.find('CategoryRow')
     expect(categoryRows.length).toBe(2)
-})
+  })
 
-it('It uses red if the item is out of stock', () => {
+  it('uses proper color if item is out of stock', () => {
     const productRows = app.find('ProductRow')
     productRows.forEach((productRow) => {
-      const isStocked = productRows.props().records.stocked
-      const domElement = productRows.render().find('tr td: first-child')
+      // productRow.render().find('tr td: first-child')
+      const isStocked = productRow.props().record.stocked
+      const domElement = productRow.render().find('tr td: first-child')
       const style = domElement[0].attribs.style
-      if(isStocked) {
+      if (isStocked) {
         expect(style).toEqual('color: black;')
-      }else{
+      } else {
         expect(style).toEqual('color: red;')
-        
       }
-      }
-    )
-}
-
+    })
+  })
+})
 
 /* to test in the command line: -- -- coverage */
