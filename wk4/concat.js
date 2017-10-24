bbe7af348ec5d64d365fa3b2dc8f3ddf4a88cd2a
michaelvincerra <michael.vincerra@gmail.com>
2017-10-23 17:42:25 -0700

    Adds concat.js warmups.

---
+++ wk4/concat.js
@@ -0,0 +1,35 @@
+//////////////////////
+//      concat()    //
+//////////////////////
+// Merges two or more arrays. This method does not change 
+// the existing arrays, but instead returns a new array.
+
+
+let arr1 = [1, 2, 3, 4, 5]
+let arr2 = [6, 7, 8, 9, 10]
+
+let arr3 = arr1.concat(arr2)
+console.log(arr3)
+console.log(arr1)
+
+
+
+let alpha = ['a', 'b', 'c']
+let alphaNums = alpha.concat(1, [2, 3])     // concatenates 3 values/ adds them to the array 
+                                            // and flattens the numbers array
+console.log(alphaNums)                      // New array generated.
+console.log(alpha)                          // Original array unchanged.
+
+
+
+// concatenating nested values; 
+
+let num1 = [[1]]
+let num2 = [2, [3]]
+
+let combined = num1.concat(num2)
+console.log(combined)
+
+console.log(num1[0].push(4))
+
+console.log(combined)
