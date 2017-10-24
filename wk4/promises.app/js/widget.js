3e145104d10aef93d820e98de942ede252b2d68a
michaelvincerra <michael.vincerra@gmail.com>
2017-10-24 16:28:40 -0700

    Adds promises.app and promises5.js to wk4.

---
+++ wk4/promises.app/js/widget.js
@@ -0,0 +1,135 @@
+function generateListItems(employees)  {
+    var statusHTML = '';
+    for (var i=0; i<employees.length; i += 1) {
+        if (employees[i].inoffice === true) {
+            statusHTML += '<li class="in">';
+        } else {
+            statusHTML += '<li class="out">';
+        }
+        statusHTML += employees[i].name;
+        statusHTML += '</li>';
+    }
+    
+    return statusHTML;
+}
+
+function generateUnorderedList(listItems) {
+    return '<ul class="bulleted">' + listItems +  '</ul>';
+}
+
+function addEmployeesToPage(unorderedList) {
+    document.getElementById('employeeList').innerHTML = unorderedList;
+}
+
+
+getJSON('../data/employees.json')
+        .then(generateListItems)
+        .then(generateUnorderedList)
+        .then(addEmployeesToPage)
+        .catch(function(e) {
+        console.log(e);
+        })
+
+
+function getJSON(url) {
+    return new Promise(function(resolve, reject){
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url)
+        xhr.onreadystatechange = handleResponse;
+        xhr.onerror = function(error) { reject(error);};
+        xhr.send();
+
+
+        function handleResponse() {
+            if(xhr.readyState === 4) {
+              if(xhr.status === 200) {
+                var employees = JSON.parse(xhr.responseText)  
+                resolve(employees);
+            } else {
+              reject(this.statusText)  
+            }
+        }
+    }
+    });
+}
+
+// const ajaxPromise = getJSON('../data/employees.json')
+
+
+// ajaxPromise.then(generatListItems)
+//             .then(generateUnorderedList)
+//             .then(addEmployeesToPage)
+//             .catch(function(e) {
+//         })
+
+// getJSON('../data/employees.json')
+//             .then(generateListItems)
+//             .then(generateUnorderedList)
+//             .then(addEmployeesToPage)
+//             .catch(function(e) {
+//             console.log(e);
+//             })
+
+
+// ORIGINAL VERSION
+
+// function getJSON(url) {
+//     return new Promise(function(resolve, reject){
+//         let xhr = new XMLHttpRequest();
+//         xhr.open('GET', url)
+//         xhr.onreadystatechange = handleResponse;
+//         xhr.oneerror = function(error) {reject(error);};
+//         xhr.send();
+
+
+//         function handleResponse() {
+//             if(xhr.readyState === 4) 
+//                 if(xhr.status === 200) {
+//                 var employees = JSON.parse(xhr.responseText);
+//                 resolve(employees)
+//             } else {
+//                 reject(this.statusText)  
+//             }
+//             };
+//     })
+// }
+
+// // const ajaxPromise = getJSON('../data/employees.json')
+
+
+// function generatListItems(employees)  {
+//     var statusHTML = '';
+//     for (var i=0; i<employees.length; i += 1) {
+//         if (employees[i].inoffice === true) {
+//             statusHTML += '<li class="in">';
+//         } else {
+//             statusHTML += '<li class="out">';
+//         }
+//         statusHTML += employees[i].name;
+//         statusHTML += '</li>';
+//     }
+    
+//     return statusHTML;
+// }
+
+// function generateUnorderedList(listItems) {
+//     return '<ul class="bulleted">' + listItems +  '</ul>';
+// }
+
+// function addEmployeesToPage(unorderedList) {
+//     document.getElementById('employeeList').innerHTML = unorderedList
+// }
+
+// // ajaxPromise.then(generatListItems)
+// //             .then(generateUnorderedList)
+// //             .then(addEmployeesToPage)
+// //             .catch(function(e) {
+// //         })
+
+// getJSON('../data/employees.json')
+//             then(generatListItems)
+//             .then(generateUnorderedList)
+//             .then(addEmployeesToPage)
+//             .catch(function(e) {
+//                 console.log(e)
+//             })
---
+++ wk4/promises5.js
@@ -0,0 +1,60 @@
+// Understanding Promises in JS
+// https://teamtreehouse.com/library/understanding-promises-in-javascript
+
+// calculation.js 
+let calculationPromise1 = new Promise (function(resolve, reject) {
+    setTimeout(function(){
+        resolve(1+1)
+    }, 1000);                       // Because this 1000, it will resolve slower: // 6
+});
+
+let calculationPromise2 = new Promise (function(resolve, reject) {
+    setTimeout(function(){
+        resolve(1+2)
+    }, 500);                        // Because this 500, it will resolve quicker: // 7
+});
+
+
+function addTwo(value){
+    return value + 2;
+}
+function printFinalValue(nextValue){
+    console.log("Final value is", nextValue)
+}
+calculationPromise1             // Unit testing is easier if chained methods are written this way.
+    .then(addTwo)
+    .then(addTwo)        
+    .then(printFinalValue)
+    
+calculationPromise2             // Unit testing is easier if chained methods are written this way.
+    .then(addTwo)
+    .then(addTwo)        
+    .then(printFinalValue)
+
+// calculationPromise.then(function(value){
+//     console.log('The answer is', value)
+// })
+// Above revised to use value in the NEXT PROMISE
+
+
+//  SEE ABOVE
+// calculationPromise.then(function(value){
+//     return value + 2; 
+// }).then(function(nextValue){
+//     console.log("Final value is", nextValue)
+// })
+
+// ABOVE functions REVISED: 
+
+// function addTwo(value){
+//     return value + 2;
+// }
+
+// function printFinalValue(nextValue){
+//     console.log("Final value is", nextValue)
+// }
+
+// calculationPromise
+//     .then(addTwo)
+//     .then(addTwo)    
+//     .then(printFinalValue)
